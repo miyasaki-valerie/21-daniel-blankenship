1289f2198ab3397dd94435e96cf42d80469d13fd
Joostin <Joostin@Joostin-PC>
2013-10-26 20:46:14 -0600

    Individual Assignment 5

--- 21/src/pkg21/Hand.java
+++ 21/src/pkg21/Hand.java
@@ -1,13 +1,33 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pkg21;
 import java.io.Serializable;
-/**
- *
- * @author Daniel Blankenship
- */
+
+
 public class Hand implements Serializable{
+   
+    String [] playersHand = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+    String [] dealerHand = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+    String playerCardOne;
+    String playerCardTwo;
+    String dealerCardOne;
+    String dealerCardTwo;
+    int max = 2;
+    public Hand(){
+        
+    }
     
+    public void displayHands(){
+       for(int c = 0; c < max; c++){
+         System.out.println(playersHand[c]);
+         System.out.println(dealerHand[c]);
+       }
+        }
+        
+    public void getPlayerHand(){
+        
+    }
+        
+    public void getDealerHand(){
+        
+    }
 }
