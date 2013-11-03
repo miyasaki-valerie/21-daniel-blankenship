7018ed929ac6c183165d8e6a66632a35fcdb28f8
Joostin <Joostin@Joostin-PC>
2013-11-02 21:58:21 -0600

    Individual Assignment 6

--- 21/src/pkg21/Hand.java
+++ 21/src/pkg21/Hand.java
@@ -5,13 +5,14 @@ import java.io.Serializable;
 
 public class Hand implements Serializable{
    
-    String [] playersHand = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+    int [] playersHand = {10,5,3};
     String [] dealerHand = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
     String playerCardOne;
     String playerCardTwo;
     String dealerCardOne;
     String dealerCardTwo;
     int max = 2;
+    int sum = 0;
     public Hand(){
         
     }
@@ -26,8 +27,22 @@ public class Hand implements Serializable{
     public void getPlayerHand(){
         
     }
+    
         
     public void getDealerHand(){
         
     }
+    
+    
+    public void addPlayerHand(){
+        for(int s : this.playersHand){
+          if(s == 10 || s == 5){
+              sum += s;
+            }
+        }
+          if(sum == 15){
+              System.out.println("Players hand total is: " + sum);
+          }
+    }
 }
+
